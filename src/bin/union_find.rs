717631d172b59e29ac835051041bbb4ffa0419a2
Steve Willson <steve.willson@gmail.com>
2022-11-09 12:06:12 -0700

    update code with timers

--- src/bin/union_find.rs
+++ src/bin/union_find.rs
@@ -32,6 +32,8 @@ fn main() {
 
 fn union_find<G: EdgeMapper>(graph: &G, nodes: u32) {
 
+    let timer = std::time::Instant::now();
+
     let mut roots: Vec<u32> = (0..nodes).collect();      // u32 works, and is smaller than uint/u64
     let mut ranks: Vec<u8> = vec![0u8; nodes as usize];  // u8 should be large enough (n < 2^256)
 
@@ -65,5 +67,6 @@ fn union_find<G: EdgeMapper>(graph: &G, nodes: u32) {
 
     let mut non_roots = 0u32;
     for i in 0..roots.len() { if i as u32 != roots[i] { non_roots += 1; }}
+    println!("Completed in:\t{:?}", timer.elapsed());
     println!("{} non-roots found", non_roots);
 }
